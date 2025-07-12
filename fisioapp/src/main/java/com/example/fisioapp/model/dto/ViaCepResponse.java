971015cacd0fca49a37436e50101fd44bf80e23d
Alexandre F Bertolin <alexandreferbertolin@gmail.com>
2025-07-12 14:06:41 -0300

    feat: finishing cep func classes

--- fisioapp/src/main/java/com/example/fisioapp/model/dto/ViaCepResponse.java
+++ fisioapp/src/main/java/com/example/fisioapp/model/dto/ViaCepResponse.java
@@ -1,23 +1,58 @@
 package com.example.fisioapp.model.dto;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.Setter;
-import lombok.ToString;
 
-@ToString
+@JsonIgnoreProperties(ignoreUnknown = true)
 @Getter
 @Setter
-@JsonIgnoreProperties(ignoreUnknown = true)
 public class ViaCepResponse {
+    @JsonProperty("cep")
     private String cep;
+
+    @JsonProperty("logradouro")
     private String logradouro;
+
+    @JsonProperty("complemento")
     private String complemento;
+
+    @JsonProperty("bairro")
     private String bairro;
+
+    @JsonProperty("localidade")
     private String localidade;
+
+    @JsonProperty("uf")
     private String uf;
+
+    @JsonProperty("ibge")
     private String ibge;
+
+    @JsonProperty("gia")
     private String gia;
+
+    @JsonProperty("ddd")
     private String ddd;
+
+    @JsonProperty("siafi")
     private String siafi;
+
+    @Override
+    public String toString() {
+        return "ViaCepResponse{" +
+                "cep='" + cep + '\'' +
+                ", logradouro='" + logradouro + '\'' +
+                ", complemento='" + complemento + '\'' +
+                ", bairro='" + bairro + '\'' +
+                ", localidade='" + localidade + '\'' +
+                ", uf='" + uf + '\'' +
+                ", ibge='" + ibge + '\'' +
+                ", gia='" + gia + '\'' +
+                ", ddd='" + ddd + '\'' +
+                ", siafi='" + siafi + '\'' +
+                '}';
+    }
+
 }
